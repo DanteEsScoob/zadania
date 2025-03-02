8e29755558fef3d57ea74745c07cdd6614869f94
DanteEsScoob <s26639@pjwstk.edu.pl>
2025-03-02 18:28:20 +0100

    Initial Project

---
+++ Nauka/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace Nauka;
+
+    class MyClass
+    {
+        static void Main(string[] args)
+        {
+            var a = 1;
+            var d = 2l;
+            var b = 1.0;
+            var c = 1.5f;
+            var done = false;
+            while (!done)
+            {
+                Console.WriteLine("Hello World!");
+            }
+            if (!done)
+            {
+                Console.WriteLine("Hello There!");
+                
+            }
+            else
+            {
+                Console.WriteLine("General Kenobi");
+            }
+
+
+        }
+    }
